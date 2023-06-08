c16ca14babe7352882dc09469e853427026e850a
Angela Zhou <a03zhou@gmail.com>
2023-06-07 22:58:55 -0400

    updated to receive message

--- play.js
+++ play.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  conn.on('data', (message) => {
+    console.log('The server message: ', message)
+  });
+
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+// code below is from cloning
 process.stdout.write('\x07');
 
 const { Game } = require('./src/Game')
@@ -9,3 +34,4 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
 game.start()
+
